8a47663ba3199c7040b7802f155516e3559e0833
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-04-30 19:40:55 +0200

    Implementing IsogramChecker

--- src/main/java/ua/skillup/isogram/IsogramChecker.java
+++ src/main/java/ua/skillup/isogram/IsogramChecker.java
@@ -1,8 +1,9 @@
 package ua.skillup.isogram;
 
+import java.util.Set;
+
 public class IsogramChecker {
     public static boolean isIsogram(String input) {
-        // implementation
-        return false;
+        return Set.of(input.toCharArray()).size() == input.length();
     }
 }
